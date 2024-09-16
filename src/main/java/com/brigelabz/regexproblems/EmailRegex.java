b39b8568ae60446da0b528be43f7f7d656bc78e3
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-16 14:27:17 +0530

    Email UC5- Finally close the expression with supporting optional parts

--- src/main/java/com/brigelabz/regexproblems/EmailRegex.java
+++ src/main/java/com/brigelabz/regexproblems/EmailRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class EmailRegex {
 
-    private static final String EMAIL_REGEX = "^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.*$";
+    private static final String EMAIL_REGEX = "^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.(\\.[a-z]{2})?$";
 
     public boolean isValidEmail(String email) {
         return Pattern.matches(EMAIL_REGEX, email);
@@ -24,9 +24,18 @@ public class EmailRegex {
         String email2= "abc+xyz@bridgelabz.co.in";
         String email3 = "abc-xyz@bridgelabz.co.in";
         String email4 = "abc#xyz@bridgelabz.co.in"; //Invalid "#" not allowed
+
+        String email5 = "abc@bridgelabz.co.in";
+        String email6 = "abc.xyz@bridgelabz.co";
+        String email7 = "abc.xyz@bridgelabz.co.uk";
+        String email8 = "abc.xyz@bridgelabz";
         regex.validateEmail(email1);
         regex.validateEmail(email2);
         regex.validateEmail(email3);
         regex.validateEmail(email4);
+        regex.validateEmail(email5);
+        regex.validateEmail(email6);
+        regex.validateEmail(email7);
+        regex.validateEmail(email8);
     }
 }
