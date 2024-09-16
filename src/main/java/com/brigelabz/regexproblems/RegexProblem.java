b4e27092d82fb2a2863d3c264bc285b37c40a910
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-16 13:39:30 +0530

    UC4- Make sure 400 088 is also valid along with 400088

--- src/main/java/com/brigelabz/regexproblems/RegexProblem.java
+++ src/main/java/com/brigelabz/regexproblems/RegexProblem.java
@@ -3,7 +3,7 @@ package com.brigelabz.regexproblems;
 
 public class RegexProblem {
 
-    static String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
+    static String PINCODE_PATTERN = "^[1-9][0-9]{2} ?[0-9]{3}$";
 
     public static boolean validatePinCode(String pincode) {
         return pincode.matches(PINCODE_PATTERN);
@@ -11,12 +11,26 @@ public class RegexProblem {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Problems");
-        String pincode = "400088B";
+        String pincode1 = "400088";
+        String pincode2 = "400 088";
+        String pincode3 = "400088B";
 
-        if (validatePinCode(pincode)) {
-            System.out.println(pincode + " is a valid pincode.");
+        if (validatePinCode(pincode1)) {
+            System.out.println(pincode1 + " is a valid pincode.");
         } else {
-            System.out.println("Invalid Pincode");
+            System.out.println("Invalid Pincode: " + pincode1);
+        }
+
+        if (validatePinCode(pincode2)) {
+            System.out.println(pincode2 + " is a valid pincode.");
+        } else {
+            System.out.println("Invalid Pincode: " + pincode2);
+        }
+
+        if (validatePinCode(pincode3)) {
+            System.out.println(pincode3 + " is a valid pincode.");
+        } else {
+            System.out.println("Invalid Pincode: " + pincode3);
         }
     }
 }
