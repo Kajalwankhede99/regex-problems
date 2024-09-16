2645062a38966330ed480845d86d3f89e9260b89
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-16 13:23:16 +0530

    UC2- Restrict pincode from taking alphates and character at beginning

--- src/main/java/com/brigelabz/regexproblems/RegexProblem.java
+++ src/main/java/com/brigelabz/regexproblems/RegexProblem.java
@@ -11,7 +11,7 @@ public class RegexProblem {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Problems");
-        String pincode = "4000788";
+        String pincode = "A400088";
 
         if (validatePinCode(pincode)) {
             System.out.println(pincode + " is a valid pincode.");
