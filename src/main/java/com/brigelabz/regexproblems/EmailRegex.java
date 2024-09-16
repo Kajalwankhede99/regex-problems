6a05d71e5e8ffdc608cdbcb5a58bb91cf132d820
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-16 13:53:49 +0530

    Email UC1- Validate Email address with regex.

---
+++ src/main/java/com/brigelabz/regexproblems/EmailRegex.java
@@ -0,0 +1,26 @@
+package com.brigelabz.regexproblems;
+
+import java.util.regex.Pattern;
+
+public class EmailRegex {
+
+    private static final String EMAIL_REGEX = "^[a-z]{3}.*$";
+
+    public boolean isValidEmail(String email) {
+        return Pattern.matches(EMAIL_REGEX, email);
+    }
+
+    public void validateEmail(String email) {
+        if (isValidEmail(email)) {
+            System.out.println("Email is Valid");
+        } else {
+            System.out.println("Email is Invalid");
+        }
+    }
+
+    public static void main(String[] args) {
+        EmailRegex regex = new EmailRegex();
+        String email = "abc.xyz@bridgelabz.co.in";
+        regex.validateEmail(email);
+    }
+}
